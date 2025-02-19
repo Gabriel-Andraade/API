a0eb0242f704060476620f6560994a2886f83c05
Gabriel-Andraade <gabriel.ap.andraade@gmail.com>
2025-02-19 11:31:28 -0300

    .

--- userRoute.js
+++ userRoute.js
@@ -1,11 +1,17 @@
-import { register, login, getUser, deleteUser, updateUser } from "./userAController.js";
+import {
+  register,
+  login,
+  getUser,
+  deleteUser,
+  updateUser,
+} from "./userAController.js";
 
 const routes = new Map([
-  ["/register", register],    
-  ["/login", login],         
-  ["/getUser", getUser],     
-  ["/deleteUser", deleteUser] 
-  ["/updateUser", updateUser]
+  ["/register", register],
+  ["/login", login],
+  ["/getUser", getUser],
+  ["/deleteUser", deleteUser],
+  ["/updateUser", updateUser],
 ]);
 
 export default routes;
